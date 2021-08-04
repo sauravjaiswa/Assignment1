fe00eb07db96870b5df485e47a1f51f763bfda73
sauravjaiswa <sauravjaiswal520@gmail.com>
2021-08-04 14:50:31 +0530

    Added a choice to see horoscope or sunsign having same DOB again

--- InputProcessor.cs
+++ InputProcessor.cs
@@ -33,57 +33,62 @@ namespace Assignment1
 
                 } while (true);
 
-                MainController controller = null;
-                bool flag = false;
                 do
                 {
-                    _logger.LogPrint("Enter choice:");
-                    _logger.LogPrint("1 for sun sign");
-                    _logger.LogPrint("2 for horoscope");
-                    _logger.LogPrint("3 for end application");
-                    ch = Console.ReadLine();
+                    MainController controller = null;
+                    bool flag = false;
+                    do
+                    {
+                        _logger.LogPrint("Enter choice:");
+                        _logger.LogPrint("1 for sun sign");
+                        _logger.LogPrint("2 for horoscope");
+                        _logger.LogPrint("3 for end application");
+                        ch = Console.ReadLine();
 
-                    if (_validation.IsValidChoice(ch))
-                    {
-                        choice = int.Parse(ch);
-                        flag = false;
-                    }
-                    else
-                    {
-                        _logger.LogError("Invalid choice!");
-                        flag = true;
-                        continue;
-                    }
-
-                    switch (choice)
-                    {
-                        case 1:
-                            controller = new MainController(new SunSign(_logger) { DOB = idob });
-                            break;
-                        case 2:
-                            controller = new MainController(new Horoscope(_logger) { DOB = idob });
-                            break;
-                        case 3:
-                            _logger.LogInfo("Ending application...");
-                            Environment.Exit(0);
-                            break;
-                        //case 4:
-                        //    controller = new MainController(new AgeCalculator(_logger) { DOB = idob });
-                        //    break;
-                        default:
+                        if (_validation.IsValidChoice(ch))
+                        {
+                            choice = int.Parse(ch);
+                            flag = false;
+                        }
+                        else
+                        {
                             _logger.LogError("Invalid choice!");
                             flag = true;
-                            break;
-                    }
-                    if(!flag)
-                        controller.DisplayResult();
-                    
-                } while (flag);
+                            continue;
+                        }
 
-                _logger.LogPrint("Enter 1 to continue: ");
-                continueChoice = Console.ReadLine();
+                        switch (choice)
+                        {
+                            case 1:
+                                controller = new MainController(new SunSign(_logger) { DOB = idob });
+                                break;
+                            case 2:
+                                controller = new MainController(new Horoscope(_logger) { DOB = idob });
+                                break;
+                            case 3:
+                                _logger.LogInfo("Ending application...");
+                                Environment.Exit(0);
+                                break;
+                            //case 4:
+                            //    controller = new MainController(new AgeCalculator(_logger) { DOB = idob });
+                            //    break;
+                            default:
+                                _logger.LogError("Invalid choice!");
+                                flag = true;
+                                break;
+                        }
+                        if (!flag)
+                            controller.DisplayResult();
+                    } while (flag);
 
-            } while (continueChoice == "1");
+                    _logger.LogPrint("Enter 1 to continue with same date");
+                    _logger.LogPrint("Enter 2 to continue with new date");
+                    _logger.LogPrint("Enter any other key to exit");
+                    continueChoice = Console.ReadLine();
+
+                } while (continueChoice == "1");
+                
+            } while (continueChoice == "2");
             _logger.LogInfo("Ending application...");
         }
     }
